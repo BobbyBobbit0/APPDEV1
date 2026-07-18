4af56406d74716e34a9faf5345dc13b19ac4aa06
bobbybobbit0 <louisebuen856@gmail.com>
2026-07-18 14:55:23 +0800

    Done with laboratory 3

---
+++ L03_getting-started-react/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+
+  return (
+    <>
+     <h1>About Me</h1>
+     <p>Name: Louise Buen</p>
+     <p>Bachelor of Science in Information System 3</p>
+     <p>Driven by my strong interest in technology, my journey in Information Systems has allowed me to build a solid foundational skillset in software development. 
+      To date, I have learned several programming languages, including Python, HTML, CSS, and Java. 
+      I have developed my coding proficiency using industry-standard tools and integrated development environments (IDEs) such as Visual Studio Code (VS Code), Eclipse, and PyCharm. 
+      Furthermore, I have expanded my technical capabilities by exploring the Laravel framework for web development. </p>
+    </>
+  )
+}
+
+export default App
